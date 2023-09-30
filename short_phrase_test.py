12af46210f6a574736cd5bb385cd8253c197412f
Ekaterina Kiyasheva <incorporated@list.ru>
2023-10-01 00:37:45 +0300

    short phrase test update

--- short_phrase_test.py
+++ short_phrase_test.py
@@ -3,7 +3,6 @@ import pytest
 class TestLineSimple:
 
     def test_short_phrase(self):
-        check_len: bool = False
         phrase = input("Set a phrase: ")
 
         assert len(phrase) < 15, f"your phrase is {len(phrase)} characters, the test expects 15 or less"
